08b8bf22826c597b38e858e89501eed5f4f82264
Gabriel Ghimici <gabriel.ghimici@teads.tv>
2019-04-07 18:40:18 +0300

    fixed tests

--- client/app/add-user/add-user.component.spec.ts
+++ client/app/add-user/add-user.component.spec.ts
@@ -1,6 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AddUserComponent } from './add-user.component';
+import {
+  MatButtonModule,
+  MatCardModule, MatDatepickerModule, MatFormFieldModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule, MatNativeDateModule, MatProgressSpinnerModule, MatSnackBarModule,
+  MatToolbarModule
+} from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AddUserActions } from '../store/add-user/add-user.actions';
+import { LogoutActions } from '../store/logout/logout.actions';
 
 describe('AddUserComponent', () => {
   let component: AddUserComponent;
@@ -8,7 +22,29 @@ describe('AddUserComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AddUserComponent ]
+      declarations: [ AddUserComponent ],
+      imports: [
+        MatToolbarModule,
+        MatMenuModule,
+        MatListModule,
+        MatIconModule,
+        MatButtonModule,
+        MatCardModule,
+        MatInputModule,
+        MatFormFieldModule,
+        MatProgressSpinnerModule,
+        MatSnackBarModule,
+        MatDatepickerModule,
+        MatNativeDateModule,
+        BrowserAnimationsModule,
+        RouterTestingModule,
+        FormsModule,
+        ReactiveFormsModule
+      ],
+      providers: [
+        AddUserActions,
+        LogoutActions
+      ]
     })
     .compileComponents();
   }));
